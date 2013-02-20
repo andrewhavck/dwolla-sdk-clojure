9eb3bd30ffb40f4d6002ee9d78804bea6de70d8b
andrewhavck <andrew.g.hauck@gmail.com>
2013-02-20 00:18:08 -0600

    Add basic info and url encoding

--- src/dwolla_sdk_clojure/core.clj
+++ src/dwolla_sdk_clojure/core.clj
@@ -1,4 +1,13 @@
-(ns dwolla-sdk-clojure.core)
+(ns dwolla-sdk-clojure.core
+  (:import [java.net URLEncoder]))
 
 (defn account_info [token]
   (str "https://www.dwolla.com/oauth/rest/users/?oauth_token=" token))
+
+(defn basic_info [client_id client_secret account_identifier]
+  (str "https://www.dwolla.com/oauth/rest/users/" account_identifier
+       "?client_id=" client_id
+       "client_secret=" client_secret))
+
+(defn encode [url]
+  (URLEncoder/encode url))
--- test/dwolla_sdk_clojure/core_test.clj
+++ test/dwolla_sdk_clojure/core_test.clj
@@ -4,4 +4,16 @@
 
 (fact "Account info request includes correct token"
   (let [token "token"]
-   (account_info token) => (str "https://www.dwolla.com/oauth/rest/users/?oauth_token=" token)))
+    (account_info token) => (str "https://www.dwolla.com/oauth/rest/users/?oauth_token=" token)))
+
+(fact "Basic info request includes correct params"
+  (let [client_id "id"
+        client_secret "secret"
+        account_identifier "a@dwolla.com"]
+    (basic_info client_id client_secret account_identifier) =>
+    (str "https://www.dwolla.com/oauth/rest/users/" account_identifier
+         "?client_id=" client_id
+         "client_secret=" client_secret)))
+
+(fact "Encode urls"
+  (encode "test url") => "test+url")
