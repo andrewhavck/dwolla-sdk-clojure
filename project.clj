d593a65b02eb4bed0710656ac423b7ef8538af6c
andrewhavck <andrew.g.hauck@gmail.com>
2013-02-19 19:11:10 -0600

    Add clj-http dependency

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "http://github.com/andrewhavck/dwolla-sdk-clojure"
   :license {:name "The MIT License (MIT)"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [clj-http "0.6.4"]])
