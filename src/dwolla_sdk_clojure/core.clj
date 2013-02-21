94b205510277eb1654f952180749d30c7c481fdc
andrewhavck <andrew.g.hauck@gmail.com>
2013-02-20 19:24:35 -0600

    Use predicate dispatch

--- src/dwolla_sdk_clojure/core.clj
+++ src/dwolla_sdk_clojure/core.clj
@@ -1,13 +1,21 @@
 (ns dwolla-sdk-clojure.core
   (:import [java.net URLEncoder]))
 
-(defn account_info [token]
+(defn- account_info [token]
   (str "https://www.dwolla.com/oauth/rest/users/?oauth_token=" token))
 
-(defn basic_info [client_id client_secret account_identifier]
+(defn- basic_info [[client_id client_secret account_identifier]]
   (str "https://www.dwolla.com/oauth/rest/users/" account_identifier
        "?client_id=" client_id
        "client_secret=" client_secret))
 
-(defn encode [url]
-  (URLEncoder/encode url))
+(defn encode [url] (URLEncoder/encode url))
+
+(defmulti req :type)
+(defmethod req :account_info [req] (encode (account_info (:msg req))))
+(defmethod req :basic_info [req] (encode (basic_info (:msg req))))
+
+
+
+
+ 
--- test/dwolla_sdk_clojure/core_test.clj
+++ test/dwolla_sdk_clojure/core_test.clj
@@ -3,17 +3,18 @@
   (:use [midje.sweet]))
 
 (fact "Account info request includes correct token"
-  (let [token "token"]
-    (account_info token) => (str "https://www.dwolla.com/oauth/rest/users/?oauth_token=" token)))
+      (let [token "token"]
+        (req {:type :account_info :msg token})
+         => (str "https%3A%2F%2Fwww.dwolla.com%2Foauth%2Frest%2Fusers%2F%3Foauth_token%3D" token)))
 
 (fact "Basic info request includes correct params"
   (let [client_id "id"
         client_secret "secret"
-        account_identifier "a@dwolla.com"]
-    (basic_info client_id client_secret account_identifier) =>
-    (str "https://www.dwolla.com/oauth/rest/users/" account_identifier
-         "?client_id=" client_id
-         "client_secret=" client_secret)))
+        account_identifier "identifier"]
+    (req {:type :basic_info :msg [client_id client_secret account_identifier]}) =>
+    (str "https%3A%2F%2Fwww.dwolla.com%2Foauth%2Frest%2Fusers%2F" account_identifier
+         "%3Fclient_id%3D" client_id
+         "client_secret%3D" client_secret)))
 
 (fact "Encode urls"
-  (encode "test url") => "test+url")
+      (encode "test url") => "test+url")
