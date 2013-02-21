347ff5cd89cff0d9189b360cce2414a2626cf30d
andrewhavck <andrew.g.hauck@gmail.com>
2013-02-20 20:29:08 -0600

    Remove encoding since clj-http does it, add json check for headers

--- src/dwolla_sdk_clojure/core.clj
+++ src/dwolla_sdk_clojure/core.clj
@@ -7,15 +7,15 @@
 (defn- basic_info [[client_id client_secret account_identifier]]
   (str "https://www.dwolla.com/oauth/rest/users/" account_identifier
        "?client_id=" client_id
-       "client_secret=" client_secret))
+       "&client_secret=" client_secret))
 
-(defn encode [url] (URLEncoder/encode url))
-
-(defmulti #^{:private true} get-api-req :type)
+(defmulti get-api-req :type)
 (defmethod get-api-req :account_info [req] (account_info  (:msg req)))
 (defmethod get-api-req :basic_info [req] (basic_info (:msg req)))
 
-(defn req [request] (encode (get-api-req request)))
+(defn json? [resp] (= "application/json; charset=utf-8" ((:headers resp) "content-type")))
+
+(defn req [req] (get-api-req req))
 
 
 
--- test/dwolla_sdk_clojure/core_test.clj
+++ test/dwolla_sdk_clojure/core_test.clj
@@ -4,17 +4,17 @@
 
 (fact "Account info request includes correct token"
       (let [token "token"]
-        (req {:type :account_info :msg token})
-         => (str "https%3A%2F%2Fwww.dwolla.com%2Foauth%2Frest%2Fusers%2F%3Foauth_token%3D" token)))
+        (get-api-req {:type :account_info :msg token})
+         => (str "https://www.dwolla.com/oauth/rest/users/?oauth_token=" token)))
 
 (fact "Basic info request includes correct params"
   (let [client_id "id"
         client_secret "secret"
         account_identifier "identifier"]
-    (req {:type :basic_info :msg [client_id client_secret account_identifier]}) =>
-    (str "https%3A%2F%2Fwww.dwolla.com%2Foauth%2Frest%2Fusers%2F" account_identifier
-         "%3Fclient_id%3D" client_id
-         "client_secret%3D" client_secret)))
+    (get-api-req {:type :basic_info :msg [client_id client_secret account_identifier]}) =>
+    (str "https://www.dwolla.com/oauth/rest/users/" account_identifier
+         "?client_id=" client_id
+         "&client_secret=" client_secret)))
 
-(fact "Encode urls"
-      (encode "test url") => "test+url")
+(fact "json? checks that the content-type is json"
+      {:headers {"content-type" "application/json; charset=utf-8"}} => json?)
