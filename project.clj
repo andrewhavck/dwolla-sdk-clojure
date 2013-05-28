3d3f152135a3b6c5d565e5049955b0bd217b211e
andrewhavck <andrew.g.hauck@gmail.com>
2013-05-27 20:26:41 -0500

    Add clojars support

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dwolla-sdk-clojure "0.1.0-SNAPSHOT"
+(defproject dwolla-sdk-clojure "0.1.0"
   :description "Dwolla API wrapper in Clojure"
   :url "http://github.com/andrewhavck/dwolla-sdk-clojure"
   :license {:name "The MIT License (MIT)"
