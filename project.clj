ef2b2f0ebec1ffe8502eebb5666ecfcc1abd14b6
Andrew Hauck <andrew@dwolla.com>
2013-08-01 12:42:47 -0500

    Update clj-http to 0.7.6

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.json "0.2.2"]
-                 [clj-http "0.7.2"]
+                 [clj-http "0.7.6"]
                  [midje "1.5.1"]
                  [bultitude "0.2.2"]
                  [lein-midje "3.0.1"]])
