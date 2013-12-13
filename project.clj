37dc87e2dca62b24958d952396e887f048119dd9
andrew hauck <andrew.g.hauck@gmail.com>
2013-12-13 17:04:23 -0600

    update dependencies

--- project.clj
+++ project.clj
@@ -4,11 +4,10 @@
   :license {:name "The MIT License (MIT)"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/data.json "0.2.2"]
-                 [clj-http "0.7.6"]]
+                 [org.clojure/data.json "0.2.3"]
+                 [clj-http "0.7.8"]]
   :profiles {:dev { :dependencies [[org.clojure/tools.namespace "0.2.4"]
                                   [org.clojure/java.classpath "0.2.1"]
-                                  [midje "1.5.1"]
+                                  [midje "1.6.0"]
                                   [bultitude "0.2.2"]
-                                  [lein-midje "3.1.1"]]}})
-
+                                  [lein-midje "3.1.3"]]}})
